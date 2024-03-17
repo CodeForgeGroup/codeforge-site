a56351767d8d7fcfa1b47508db4b99909cfe3ed2
Kellinton <kellintonbrasil@gmail.com>
2024-03-17 16:56:14 -0300

    feature: Página Sobre terminada

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SobreController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,4 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/sobre', [SobreController::class, 'index'])->name('sobre');
