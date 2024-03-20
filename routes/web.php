f15b8063393bca0417665a7172675575ab8463cf
Kellinton <kellintonbrasil@gmail.com>
2024-03-20 09:32:07 -0300

    Página/contato terminada o layout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ServicoController;
 use App\Http\Controllers\SobreController;
@@ -24,3 +25,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/sobre', [SobreController::class, 'index'])->name('sobre');
 Route::get('/servico', [ServicoController::class, 'index'])->name('servico');
+Route::get('/contato', [ContatoController::class, 'index'])->name('contato');
